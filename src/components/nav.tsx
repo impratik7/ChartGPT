9be64e260b8d5a16c62be8cef3dd49d560f0224b
USCS-XXX <USCS-XXX@scs.com>
2023-09-19 10:30:07 -0700

    upload proj

---
+++ src/components/nav.tsx
@@ -0,0 +1,31 @@
+import { Icon } from "@iconify/react";
+import Image from "next/image";
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div className="fixed top-0 left-0 z-50 w-full">
+      <nav className="footer items-center p-1 text-neutral-content">
+        <div className="items-center grid-flow-col">
+          <a
+            className="btn btn-ghost"
+            href="https://www.superkeys.app/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <Image
+              src="/brand/logo_text.png"
+              width={100}
+              height={20}
+              alt="Flowgpt Logo"
+            />
+            {/* <div className="badge badge-sm self-end">alpha</div> */}
+          </a>
+        </div>
+
+      </nav>
+    </div>
+  );
+};
+
+export default Nav;
